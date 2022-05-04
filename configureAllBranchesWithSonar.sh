75151058b9d8ce79b076f944a6748a83cad961ce
Torkin <daniele.laprova@hotmail.it>
2022-05-05 01:45:31 +0200

    add sonarcloud config to all repo

--- configureAllBranchesWithSonar.sh
+++ configureAllBranchesWithSonar.sh
@@ -1,11 +1,9 @@
 #! /bin/sh
 
-cp -r ../.github --remove-destination .
-
 for remote in $(git branch -r)
 do 
     git checkout  --track $remote
-    cp -r ../.github --remove-destination .github
+    cp -r ../.github --remove-destination .
     git add .github
     git commit -a -m "add sonarcloud config to all repo"
     git push origin $remote
