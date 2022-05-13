650672d36a29fce1465108d8ef55e243956597cc
Torkin <daniele.laprova@hotmail.it>
2022-05-13 20:03:44 +0200

    Add jacoco code coverage #7

--- makeBranchesFromTagsAndPushThem.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-
-RELEASES=$(git tag)
-
-for RELEASE in $RELEASES
-do
-    git branch "branch-$RELEASE" $RELEASE
-done
-git push --all -u
--- removeAllLocalAndRemoteBranches.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/sh
-
-git branch | grep -v "main" | xargs git branch -D
-git branch -r | grep origin/ | grep -v 'master$' | grep -v HEAD| cut -d/ -f2 | while read line; do git push origin :$line; done;
